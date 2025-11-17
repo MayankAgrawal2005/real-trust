aa4a91efbaf56e68c2f872b28ea3b7e94cdc29f6
Mayank Agrawal <MayankAgrawal2005>
2025-11-18 02:30:27 +0530

    axios value change

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // change to deployed URL later
+const API_BASE_URL = "https://real-trust-7.onrender.com/api"; // change to deployed URL later
 
 export const api = axios.create({
   baseURL: API_BASE_URL
